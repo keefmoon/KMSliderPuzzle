b80effb87d90caaab273b446e0b07070b08f0875
Keith Moon <keef@keefmoon.com>
2012-05-02 00:11:50 +0100

    Game options view added and tapping patch will execute valid move.

---
+++ KMSliderPuzzle/KMSliderPuzzle/ViewControllers/KMGameOptionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  KMGameOptionsViewController.h
+//  KMSliderPuzzle
+//
+//  Created by Keith Moon on 01/05/2012.
+//  Copyright (c) 2012 Data Ninjitsu Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMGameOptionsViewController : UIViewController
+
+@end
